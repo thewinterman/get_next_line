c61064a3595431087c0a89093ae4d4ab95a6d931
Winter <matwinte.42@bluewin.ch>
2022-11-13 22:11:05 +0100

    initial commit

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: matwinte <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/13 22:02:33 by matwinte          #+#    #+#             */
+/*   Updated: 2022/11/13 22:07:55 by matwinte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h> //read
+# include <stdlib.h> //malloc, free
+
+char	*get_next_line(int fd);
+
+#endif
